1b5ccfdbd63de28dfec22262314f87ba0a857731
muit <muit@piperift.com>
2021-11-07 13:29:35 +0100

    Added bezier function

---
+++ Include/Math/Bezier.h
@@ -0,0 +1,13 @@
+// Copyright 2015-2021 Piperift - All rights reserved
+
+#pragma once
+
+#include "PCH.h"
+
+#include "Math/Vector.h"
+
+
+namespace Rift::Bezier
+{
+	CORE_API v2 EvaluateCubic(v2 p0, v2 p1, v2 p2, v2 p3, float t);
+}    // namespace Rift::Bezier
