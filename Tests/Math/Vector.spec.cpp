bb6970379063fc3ef77bd3640ca1dc9c5f588902
muit <muit@piperift.com>
2022-06-10 17:28:48 +0200

    Added vector test

--- Tests/Math/Vector.spec.cpp
+++ Tests/Math/Vector.spec.cpp
@@ -7,16 +7,38 @@
 #include <limits>
 
 
+using namespace snowhouse;
+using namespace bandit;
 using namespace p;
 
 
 go_bandit([]() {
 	describe("Math.Vector", []() {
-		it("LowerBound", [&]() {
+		it("Can reflect", [&]() {
 			p::v2 v{0.f, 1.f};
 			p::v2 normal{1.f, 0.f};
 			p::v2 v2 = v.Reflect(normal);
-			AssertThat(v.Equals(v2), Equals(true));
+			AssertThat(v2.Equals({0.f, 1.f}), Equals(true));
+			v      = p::v2{0.f, 1.f};
+			normal = p::v2{0.f, 1.f};
+			v2     = v.Reflect(normal);
+			AssertThat(v2.Equals({0.f, -1.f}), Equals(true));
+			v      = p::v2{1.f, 1.f};
+			normal = p::v2{0.f, 1.f};
+			v2     = v.Reflect(normal);
+			AssertThat(v2.Equals({1.f, -1.f}), Equals(true));
+			v      = p::v2{1.f, 1.f};
+			normal = p::v2{1.f, 0.f};
+			v2     = v.Reflect(normal);
+			AssertThat(v2.Equals({-1.f, 1.f}), Equals(true));
+			v      = p::v2{-1.f, 1.f};
+			normal = p::v2{-1.f, 0.f};
+			v2     = v.Reflect(normal);
+			AssertThat(v2.Equals({1.f, 1.f}), Equals(true));
+			v      = p::v2{-1.f, -1.f};
+			normal = p::v2{0.f, 1.f};
+			v2     = v.Reflect(normal);
+			AssertThat(v2.Equals({-1.f, 1.f}), Equals(true));
 		});
 	});
 });
