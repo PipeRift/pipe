5fe29da3dc261193bd5d4f38c1bd0b61c305a785
muit <muit@piperift.com>
2022-07-15 23:56:52 +0200

    Small optimization

--- Src/Memory/MultiLinearArena.cpp
+++ Src/Memory/MultiLinearArena.cpp
@@ -37,7 +37,7 @@ namespace p
 		}
 
 		--block->count;
-		if (block->count == 0 && block != freeBlock) [[unlikely]]
+		if (block->count <= 0) [[unlikely]]
 		{
 			// If the block is empty and was marked full, free it
 			FreeBlock(parentArena, block);
@@ -52,16 +52,20 @@ namespace p
 		// Allocate aligning by blockSize
 		void* ptr = p::Alloc(parentArena, blockSize + sizeof(LinearBlock), GetBlockSize());
 
-		void* blockPtr = (u8*)ptr + GetAlignmentPadding(ptr, GetBlockSize());
-		freeBlock      = new (blockPtr) LinearBlock();
-		insert         = freeBlock + 1;
-
+		void* blockPtr       = (u8*)ptr + GetAlignmentPadding(ptr, GetBlockSize());
+		freeBlock            = new (blockPtr) LinearBlock();
+		insert               = freeBlock + 1;
+		freeBlock->count     = 1;    // Count +1 prevents Free cleaning blocks being filled
 		freeBlock->last      = lastBlock;
 		freeBlock->unaligned = ptr;
 
 		if (lastBlock)
 		{
 			lastBlock->next = freeBlock;
+			// Dismark block allowing freeing it:
+			--lastBlock->count;
+			// In theory, we would need to free lastBlock if full here...
+			// But we only reach AllocateBlock if the previous one is full!
 		}
 	}
 
