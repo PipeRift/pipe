86044032ea74eef300aa2206652688dbc72661a4
muit <muit@piperift.com>
2022-09-01 22:20:36 +0200

    Moved hierarchy from Rift

---
+++ Include/Pipe/ECS/Components/CParent.h
@@ -0,0 +1,27 @@
+// Copyright 2015-2022 Piperift - All rights reserved
+#pragma once
+
+#include "Pipe/Core/Array.h"
+#include "Pipe/ECS/Id.h"
+#include "Pipe/Reflect/Struct.h"
+
+
+namespace p::ecs
+{
+	struct PIPE_API CParent : public Struct
+	{
+		STRUCT(CParent, Struct)
+
+		PROP(children)
+		TArray<Id> children;
+	};
+
+	static void Read(Reader& ct, CParent& val)
+	{
+		ct.Serialize(val.children);
+	}
+	static void Write(Writer& ct, const CParent& val)
+	{
+		ct.Serialize(val.children);
+	}
+}    // namespace p::ecs
