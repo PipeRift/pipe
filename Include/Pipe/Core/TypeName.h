8acfc00ec3d4da0fbd535361b6735e8cfad432fc
muit <muit@piperift.com>
2024-05-19 19:08:49 +0200

    Account for templates in typenames

--- Include/Pipe/Core/TypeName.h
+++ Include/Pipe/Core/TypeName.h
@@ -31,22 +31,54 @@ namespace p
 
 	inline constexpr StringView RemoveNamespace(StringView value)
 	{
-		// TODO: Detect '<' for templates
-		const sizet pos = Strings::Find(value, "::", FindDirection::Back);
-		if (pos != StringView::npos)
+		if (value.size() > 0)
 		{
-			return Strings::RemoveFromStart(value, pos + 2);
+			const char* c               = value.data();
+			const char* const end       = c + value.size();
+			const char* const last      = end - 1;
+			const char* lastFoundQuotes = nullptr;
+			while (c < last)    // skip last char
+			{
+				if (*c == ':' && *(c + 1) == ':')
+				{
+					lastFoundQuotes = c;
+					++c;    // Skip one more character for ::
+				}
+				else if (*c == '<')    // Stop on templates
+					break;
+				++c;
+			}
+			if (lastFoundQuotes)
+			{
+				return {lastFoundQuotes + 2, end};
+			}
 		}
 		return value;
 	}
 	inline constexpr StringView RemoveNamespace(StringView value, StringView& outNamespace)
 	{
-		// TODO: Detect '<' for templates
-		const sizet pos = Strings::Find(value, "::", FindDirection::Back);
-		if (pos != StringView::npos)
+		if (value.size() > 0)
 		{
-			outNamespace = {value.data(), pos};
-			return Strings::RemoveFromStart(value, pos + 2);
+			const char* c               = value.data();
+			const char* const end       = c + value.size();
+			const char* const last      = end - 1;
+			const char* lastFoundQuotes = nullptr;
+			while (c < last)    // skip last char
+			{
+				if (*c == ':' && *(c + 1) == ':')
+				{
+					lastFoundQuotes = c;
+					++c;    // Skip one more character for ::
+				}
+				else if (*c == '<')    // Stop on templates
+					break;
+				++c;
+			}
+			if (lastFoundQuotes)
+			{
+				outNamespace = {value.data(), lastFoundQuotes};
+				return {lastFoundQuotes + 2, end};
+			}
 		}
 		outNamespace = {};
 		return value;
@@ -61,6 +93,7 @@ namespace p
 
 		typeName = Strings::RemoveFromStart(typeName, "struct ");
 		typeName = Strings::RemoveFromStart(typeName, "class ");
+		typeName = Strings::RemoveFromStart(typeName, "enum ");
 
 		if (!includeNamespaces)
 		{
